9bbbf280406c79c69e771f17ad56d85a5457177e
chaewon0228 <s2033@e-mirim.hs.kr>
2021-08-13 23:44:54 +0900

    intro 3ms Runner

--- app/src/main/java/com/example/doctorcommunication/SplashActivity.java
+++ app/src/main/java/com/example/doctorcommunication/SplashActivity.java
@@ -1,16 +1,36 @@
 package com.example.doctorcommunication;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+
 import androidx.appcompat.app.AppCompatActivity;
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends Activity {
+    @SuppressLint("ResourceType")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_view);
 
+        Handler handler = new Handler();
 
-        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-        startActivity(intent);
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+        // 3초 뒤에 Runner 객체 실행
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
         finish();
     }
 }
