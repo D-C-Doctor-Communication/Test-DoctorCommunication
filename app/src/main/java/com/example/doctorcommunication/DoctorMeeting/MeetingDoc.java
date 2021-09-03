5cf680e4e41734a60b36aa4b6810479e783f8908
KimHaNeul <s2025@e-mirim.hs.kr>
2021-09-04 02:25:17 +0900

    의사와의 만남 코드 수정

--- app/src/main/java/com/example/doctorcommunication/DoctorMeeting/MeetingDoc.java
+++ app/src/main/java/com/example/doctorcommunication/DoctorMeeting/MeetingDoc.java
@@ -23,7 +23,9 @@ import com.example.doctorcommunication.MainActivity;
 import com.example.doctorcommunication.R;
 import com.example.doctorcommunication.Recording.Recording;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.Year;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
@@ -59,6 +61,9 @@ public class MeetingDoc extends AppCompatActivity {
         //툴바 구성 - 이전버튼, 녹음버튼있
         Toolbar toolbar = findViewById(R.id.toolbar_meeting_doctor); //마이크, 이전버튼 들어있는 toolbar 생성
         setSupportActionBar(toolbar);
+        startDate = Calendar.getInstance();
+        endDate = Calendar.getInstance();
+
 
 
         // 증상에 대한 심각도 그래프로 이동하는 버튼
@@ -76,12 +81,13 @@ public class MeetingDoc extends AppCompatActivity {
             gotoGraph.commit();
         });
 
+        //날짜선택 버튼 위 증상 텍스트
+        symptom_title = findViewById(R.id.symptom_title);
         //기간 선택을 위한 DatePicker를 호출하는 버튼(TextView)
         startDateText = (TextView) findViewById(R.id.startDate);
         endDateText = (TextView) findViewById(R.id.endDate);
 
-        //날짜선택 버튼 위 증상 텍스트
-        symptom_title = findViewById(R.id.symptom_title);
+
         //증상 선택 버튼
         for (int i = 0; i < buttonKey.length; i++) {
             symptomBtn[i] = findViewById(buttonKey[i]);
@@ -111,19 +117,13 @@ public class MeetingDoc extends AppCompatActivity {
             createDCList(2, dcListView);
         });
 
+
 // -> 날짜 선택 기능
-
-        //기본 날짜 텍스트 오늘로 설정
-        Calendar calendar = Calendar.getInstance();
-        int mYear = calendar.get(Calendar.YEAR);
-        int mMonth = calendar.get(Calendar.MONTH);
-        int mDay = calendar.get(Calendar.DAY_OF_MONTH);
-        startDateText.setText(mYear + "." + (mMonth + 1) + "." + mDay);
-        endDateText.setText(mYear + "." + (mMonth + 1) + "." + mDay);
-
+        //기본날짜 오늘로 지정
+        setDateText(startDate);
+        setDateText(endDate);
         //시작날짜 DatePickerDialog 동작
         startDateText.setOnClickListener(v -> {
-
             //DatePickerDialog 객체 생성
             DatePickerDialog datePickerDialog = new DatePickerDialog(
                     this,
@@ -131,36 +131,32 @@ public class MeetingDoc extends AppCompatActivity {
                     (view, year, month, dayOfMonth) -> {
                         String PickedDate = year + "." + (month + 1) + "." + dayOfMonth;
                         startDateText.setText(PickedDate);
+                        startDate.set(year,month+1,dayOfMonth);
                     }
                     //기본 세팅 날짜 지정 (위의 변수대로)
-                    , mYear, mMonth, mDay
+                    , startDate.get(Calendar.YEAR), startDate.get(Calendar.MONTH), startDate.get(Calendar.DAY_OF_MONTH)
             );
             //DatePickerDialog 표시
             datePickerDialog.show();
-
         });
 
         //종료날짜 DatePickerDialog 동작
         endDateText.setOnClickListener(v -> {
-
-            //DatePickerDialog 불러옴
             DatePickerDialog datePickerDialog = new DatePickerDialog(
                     this,
-                    //0000.00.00의 형식으로 입력받은 날짜를 endDate 텍스트뷰의 텍스트로 지정
                     (view, year, month, dayOfMonth) -> {
                         String PickedDate = year + "." + (month + 1) + "." + dayOfMonth;
                         endDateText.setText(PickedDate);
+                        endDate.set(year,month+1,dayOfMonth);
                     }
-                    //기본 세팅 날짜 지정 (위의 변수대로)
-                    , mYear, mMonth, mDay
+                    , endDate.get(Calendar.YEAR), endDate.get(Calendar.MONTH), endDate.get(Calendar.DAY_OF_MONTH)
             );
-            //DatePickerDialog 표시
             datePickerDialog.show();
         });
     }
 
 
-    // -> toolbar 기능
+// -> toolbar 기능
     @Override
     //toolbar와 메뉴 구성 layout 연결
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -188,17 +184,12 @@ public class MeetingDoc extends AppCompatActivity {
     //클릭한 버튼의 증상명과 데이터의 증상이 일치한지 확인
     public boolean checkSymptom(int index, String part) {
         //버튼별 증상 배열
-        String[] btnString = new String[]{"가래", "복통", "두통"};
+        String[] btnString = new String[]{"두통", "복통", "요통"};
         if (part.equals(btnString[index])) return true;
         return false;
     }
 
-    //텍스트 지정
-    //public void setCalendarDate(Calendar calendarDate,TextView textView){
-
-    //}
-
-
+//db연동필요 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     //기록 리스트 생성하는 메소드
     public void createDCList(int index, ListView listView) {
         //listView 참조 및 Adapter 연결
@@ -218,38 +209,39 @@ public class MeetingDoc extends AppCompatActivity {
         adapter.notifyDataSetChanged();
     }
 
+    //날짜 초기값 지정
+    public void setDateText(Calendar cal){
+        //Calendar cal = Calendar.getInstance();
+        int mYear = cal.get(Calendar.YEAR);
+        int mMonth = cal.get(Calendar.MONTH);
+        int mDay = cal.get(Calendar.DAY_OF_MONTH);
+        startDateText.setText(mYear + "." + (mMonth + 1) + "." + mDay);
+        endDateText.setText(mYear + "." + (mMonth + 1) + "." + mDay);
+        cal.set(mYear,mMonth+1,mDay);
+    }
 
-    //데이터의 날짜가 datePicker의 두 날짜 사이에있으면 true 반환
-    public boolean checkIsBetween(String date) {
+
+    public boolean checkIsBetween(String date){
         try {
-            //startDate,endDate : DatePicker에서 선택한 시작/종료 날짜
-            //wantCheck : 증상이 기록된 날짜
-            String start = startDateText.getText().toString();
-            String end = endDateText.getText().toString();
-
-            Calendar StartDay, EndDay, CheckDate; //Date객체형으로 바꾼 시작 / 끝 날짜
-            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy.MM.dd", Locale.KOREA); //형변환을 위한 데이트포멧 객체 생성
-
-            Date date_start = dateFormat.parse(start); //Date형으로 변환
-            Date date_end = dateFormat.parse(end);
+            //string형 날짜를 calendar형으로 변환
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy.MM.dd", Locale.KOREA);
             Date date_wantCheck = dateFormat.parse(date);
+            Calendar checkDate = Calendar.getInstance();
+            checkDate.setTime(date_wantCheck);
 
-            StartDay = Calendar.getInstance(); //Calender형으로 변환
-            StartDay.setTime(date_start);
-            EndDay = Calendar.getInstance();
-            EndDay.setTime(date_end);
-            CheckDate = Calendar.getInstance();
-            CheckDate.setTime(date_wantCheck);
-
-            if ((CheckDate.before(EndDay) || CheckDate.equals(EndDay)) && !CheckDate.before(StartDay)) {
-                Log.d("myapp", "통과됨");
-                return true;
+            //날짜가 범위 사이에 있는지 확인
+            if (checkDate.compareTo(startDate) >= 0) {
+                if (checkDate.compareTo(endDate) <= 0) {
+                    return true;
+                }
             }
-            Log.d("myapp", "통과되지 않음");
-            return false;
-        } catch (Exception e) {
-            Log.d("myapp", "예외 발생");
             return false;
         }
+        catch (ParseException e){
+            Log.d("myapp", "예외 발생"+date);
+        }
+        return false;
+        //Log.d("myapp",startDate.get(Calendar.YEAR)+","+startDate.get(Calendar.MONTH)+","+startDate.get(Calendar.DAY_OF_MONTH));
+        //Log.d("myapp",endDate.get(Calendar.YEAR)+","+endDate.get(Calendar.MONTH)+","+endDate.get(Calendar.DAY_OF_MONTH));
     }
 }
